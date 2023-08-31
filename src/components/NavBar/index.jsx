45d7c17404b4b7a95794a2ddede093070ccee733
matias rosas <matiasdanielrosas@gmail.com>
2023-08-30 21:11:44 -0300

    Routing Agregado

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,18 +3,23 @@ import CartWidget from "../CartWidget";
 import {ReactComponent as ReactLogo} from '../../img/Bairesvector.svg';
 import { faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Dropdown from "../Dropdown";
 import "./style.css";
+import { Link } from "react-router-dom";
 
 
 
 export default function NavBar() {
+
   return (
     <div className="colorNav">
-    <a href="#"><ReactLogo /></a>
+    <Link to="/"><ReactLogo /></Link>
       <ul className="navList">
-      <a href="#"><li>Nosotros</li></a>
-      <a href="#"><li>Instrumentos</li></a> 
-      <a href="#" className="buscar"><li>Buscar<FontAwesomeIcon icon={faMagnifyingGlass} /></li></a>  
+      <Link to="/"><li>Nosotros</li></Link>
+       <div className="dropdown">
+      <Dropdown />
+    </div> 
+      <Link to="/" className="buscar"><li>Buscar<FontAwesomeIcon icon={faMagnifyingGlass} /></li></Link>  
         </ul>
       <div className="carrito">
         <CartWidget />
